9a90ed1a0ddbfa0a6e0a88bfcd8091323872c03f
PinkyPotrick <62207615+PinkyPotrick@users.noreply.github.com>
2022-03-29 16:35:08 +0300

    Fixed Tests Lab02

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -7,3 +7,11 @@ l
 <src/main/java/inventory/controller/MainScreenController.java,f\e\fe123040025ffc7790be0315cb187fb9b2631761
 \
 ,src/main/java/inventory/model/Inventory.java,6\f\6f314193286495a02fb473e4cf32b85ed85f7dcf
+b
+2src/main/java/inventory/controller/Controller.java,a\6\a6635e298a8cf86a761fdee9b7ec350061490945
+7
+pom.xml,4\4\442292b8a7efeabbe4cc176709b833b1792140ec
+^
+.src/main/java/inventory/model/InhousePart.java,6\6\66f68dc77d37d8374d575a22cff106c783fe7fc9
+e
+5src/main/java/inventory/service/InventoryService.java,f\b\fbfefaa7b63a08d0319b42efe2f7604a9cd84bce
--- src/test/java/inventory/repository/InventoryRepositoryTest.java
+++ src/test/java/inventory/repository/InventoryRepositoryTest.java
@@ -10,6 +10,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -21,23 +22,18 @@ class InventoryRepositoryTest {
     int added, added2;
 
     @BeforeAll
-    void createRepo(){
-        repository = new InventoryRepository();
-        list_parts = new ArrayList<Part>();
-        list_parts.add(new Part(0, "working_part", 1.00, 50, 5, 100));
-        list_parts.add(new Part(2, "", 1.00, 10, 5, 100));
-        list_parts.add(new Part(1, "working_part2", 1.00, 40, 5, 100));
-        list_parts.add(new Part(3, "Compresor", 1.00, 101, 5, 100));
+    void initCounter() {
+        counter = 0;
         added = 0;
         added2 = 0;
-        assertEquals(4, list_parts.size(), "Not all 4 parts have been added");
-        counter = 0;
         System.out.println("Mock repository created");
     }
-@Disabled
+
     @Test
     @Order(1)
-    void createParts() {
+    void createList() {
+        list_parts = new ArrayList<Part>();
+
         list_parts.add(new Part(0, "working_part", 1.00, 50, 5, 100));
         list_parts.add(new Part(2, "", 1.00, 10, 5, 100));
         list_parts.add(new Part(1, "working_part2", 1.00, 40, 5, 100));
@@ -48,109 +44,58 @@ class InventoryRepositoryTest {
 
     @Test
     @Order(2)
-    @Disabled("These tests have flaws")
-    void addIncorrectPartsInRepo() {
-        list_parts = new ArrayList<Part>();
-        counter = 0;
-        assertEquals("", Part.isValidPart("working_part", 1.00, 50, 5, 100, ""), "The part was not correctly defined");
-        assertEquals("", Part.isValidPart("", 1.00, 50, 5, 100, ""), "The name has less than 1 character");
-        assertEquals("", Part.isValidPart("working_part", 1.00, 40, 5, 100, ""), "The part was not correctly defined");
-        assertEquals("", Part.isValidPart("Compresor", 1.00, 101, 5, 100, ""), "The inStock number is higher than the maximum allowed for storage");
+    void addIncorrectPartsInRepoECP() {
+        InventoryRepository inventoryRepository = new InventoryRepository();
 
-        if(Part.isValidPart("working_part", 1.00, 10, 5, 100, "").equals("")) {
-            counter += 1;
-            repository.addPart(list_parts.get(counter));
+        inventoryRepository.addPart(new Part(0, "working_part", 1.00, 50, 5, 100));
+        if (Objects.equals(Part.isValidPart("", 1.00, 10, 5, 100, ""), "")) {
+            inventoryRepository.addPart(new Part(2, "", 1.00, 10, 5, 100));
         }
-        if(Part.isValidPart("", 1.00, 10, 5, 100, "").equals("")) {
-            counter += 1;
-            repository.addPart(list_parts.get(counter));
+        inventoryRepository.addPart(new Part(1, "working_part2", 1.00, 40, 5, 100));
+        if (Objects.equals(Part.isValidPart("Compresor", 1.00, 101, 5, 100, ""), "")) {
+            inventoryRepository.addPart(new Part(3, "Compresor", 1.00, 101, 5, 100));
         }
-        if(Part.isValidPart("working_part2", 1.00, 40, 5, 100, "").equals("")) {
-            counter += 1;
-            repository.addPart(list_parts.get(counter));
-        }
-        if(Part.isValidPart("Compresor", 1.00, 101, 5, 100, "").equals("")) {
-            counter += 1;
-            repository.addPart(list_parts.get(counter));
-        }
+
+        assertEquals(2, inventoryRepository.getAllParts().size(), "The invalid part objects have been incorrectly added.");
     }
 
     @Test
-    @Order(2)
-    //@Disabled("These tests do NOT have flaws")
-    void addCorrectPartsInRepo() {
-        repository = new InventoryRepository();
-        counter = 0;
-        assertEquals("", Part.isValidPart("working_part", 1.00, 50, 5, 100, ""), "The part was not correctly defined");
-        assertEquals("", Part.isValidPart("Some_other_working_part", 1.00, 50, 5, 100, ""), "The part was not correctly defined");
-        assertEquals("", Part.isValidPart("working_part", 1.00, 40, 5, 100, ""), "The part was not correctly defined");
-        assertEquals("", Part.isValidPart("Some_other_working_part", 1.00, 20, 5, 100, ""), "The part was not correctly defined");
-
-        if(Part.isValidPart("working_part", 1.00, 10, 5, 100, "").equals("")) {
-
-            repository.addPart(list_parts.get(counter));
-            counter += 1;
-        }
-        if(Part.isValidPart("Some_other_working_part", 1.00, 50, 5, 100, "").equals("")) {
-
-            repository.addPart(list_parts.get(counter));
-            counter += 1;
-        }
-        if(Part.isValidPart("working_part2", 1.00, 40, 5, 100, "").equals("")) {
-
-            repository.addPart(list_parts.get(counter));
-            counter += 1;
-        }
-        if(Part.isValidPart("Some_other_working_part", 1.00, 20, 5, 100, "").equals("")) {
-
-            repository.addPart(list_parts.get(counter));
-            counter += 1;
-        }
-    }
-
-    @AfterAll
-    @Test
-    void verifyAddedParts() {
-        assertEquals(repository.getAllParts().size(), counter, "We do not have " + counter.toString()  + " items in stock");
-    }
-
-    @ParameterizedTest
     @Order(3)
-    @ValueSource(strings = {"working_part", "", "working_part2", "Compresor"})
-    void addPart(String val) {
-        if (val.equals("working_part")){
-            counter = 0;
-            repository = new InventoryRepository();
-    }
-        Part p = list_parts.get(counter);
-        if(Part.isValidPart(val, p.getPrice(), p.getInStock(), p.getMin(), p.getMax(), "").equals("")) {
-            repository.addPart(p);
-            added += 1;
-        }
-        counter += 1;
-        if (repository.getAllParts().size() == counter - added + 1) {
-            assertEquals(repository.getAllParts().size(), counter - added + 1, "We do not have " +added  + " items in stock");
-        }
-        //assertEquals(repository.getAllParts().size(), counter - added + 1, "We do not have " +added  + " items in stock");
+    void addCorrectPartsInRepoECP() {
+        InventoryRepository inventoryRepository = new InventoryRepository();
+
+        inventoryRepository.addPart(new Part(0, "working_part", 1.00, 50, 5, 100));
+        inventoryRepository.addPart(new Part(2, "Some_other_working_part", 1.00, 10, 5, 100));
+        inventoryRepository.addPart(new Part(1, "working_part2", 1.00, 40, 5, 100));
+        inventoryRepository.addPart(new Part(3, "Some_other_working_part", 1.00, 20, 5, 100));
+
+        assertEquals(4, inventoryRepository.getAllParts().size(), "The invalid part objects have been incorrectly added.");
     }
 
-    @ParameterizedTest
+    @Test
     @Order(4)
-    @ValueSource(ints = {5, 4, 90, 105})
-    void addPart2(Integer val){
-        if(val == 5){
-            counter = 0;
-            repository = new InventoryRepository();
-        }
-        Part p = list_parts.get(counter);
-        if(Part.isValidPart(p.getName(), p.getPrice(), val, p.getMin(), p.getMax(), "").equals("")) {
-            repository.addPart(p);
-            added2 += 1;
-        }
-        counter += 1;
-        if (repository.getAllParts().size() == counter - added2 + 1) {
-            assertEquals(repository.getAllParts().size(), counter - added2 + 1, "We do not have " +added  + " items in stock");
-        }
-        //assertEquals(repository.getAllParts().size(), counter - added2 + 1, "We do not have " + added2  + " items in stock");
+    @Disabled("These tests have flaws")
+    void addIncorrectPartsInRepoBVA() {
+        InventoryRepository inventoryRepository = new InventoryRepository();
+
+        inventoryRepository.addPart(new Part(0, "Compresor", 3000.00, 50, 5, 100));
+        inventoryRepository.addPart(new Part(2, "", 3000.00, 10, 5, 100));
+        inventoryRepository.addPart(new Part(1, "Compresor", 3000.00, 40, 5, 100));
+        inventoryRepository.addPart(new Part(3, "Compresor", 3000.00, 2, 5, 100));
+
+        assertEquals(2, inventoryRepository.getAllParts().size(), "The invalid part objects have been incorrectly added.");
+    }
+
+    @Test
+    @Order(5)
+    void addCorrectPartsInRepoBVA() {
+        InventoryRepository inventoryRepository = new InventoryRepository();
+
+        inventoryRepository.addPart(new Part(0, "Compresor", 3000.00, 50, 5, 100));
+        inventoryRepository.addPart(new Part(2, "Compresor", 3000.00, 10, 5, 100));
+        inventoryRepository.addPart(new Part(1, "Compresor", 3000.00, 40, 5, 100));
+        inventoryRepository.addPart(new Part(3, "Compresor", 3000.00, 20, 5, 100));
+
+        assertEquals(4, inventoryRepository.getAllParts().size(), "The invalid part objects have been incorrectly added.");
     }
 }
